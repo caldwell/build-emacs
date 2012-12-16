ace352e0440726aadef35eb8c68aab98493de8ce
David Caldwell <david@porkrind.org>
2012-12-16 04:04:05 -0800

    Add ability to compile with the 10.7 SDK by pointing an env variable to the 10.7 Xcode.app.
    This enables the Lion fullscreen feature available in recent code.
    
    The binary built with the 10.7 SDK doesn't get lipo-ed in to the main emacs
    (since there's already a 10.5 SDK based x86_64 binary). Instead, the main
    Emacs executable is now a little script that chooses the correct binary
    based on the current OS and execs the closes version that exists (without
    going over).
    
    This means you can't gdb Emacs.app/Contents/MacOS/Emacs directly any more,
    but that seems like a small price to pay. I can't think of any other
    downsides to this technique.

---
+++ launch.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+def numify(version_string)
+  version_string.sub(/^(\d+)\.(\d+)(?:\.(\d+))?$/) { |m| $1.to_i*10000 + $2.to_i*100 + ($3 || "0").to_i*1 }.to_i
+end
+
+def osascript(script)
+  system 'osascript', *script.split(/\n/).map { |line| ['-e', line] }.flatten
+end
+
+
+versions = Hash[Dir["#{$0}-*"].map { |file| [ numify(file.sub(/^.*-(.+)$/, '\1')), file] }]
+version = numify(`sw_vers -productVersion`)
+if highest_compatible_version =  versions.keys.select { |v| v <= version }.max
+  exec versions[highest_compatible_version], *ARGV
+end
+
+osascript <<-ENDSCRIPT
+  tell application "Finder"
+    activate
+    display dialog "This application will not run on your computer. Sorry!"
+  end tell
+ENDSCRIPT
