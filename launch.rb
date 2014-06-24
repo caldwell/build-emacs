a59c1e12d26043e775c2b805dcdae1eaaa1751ea
David Caldwell <david@porkrind.org>
2014-06-24 12:28:53 -0700

    [combine-and-package,launch.rb] Segregate arch/version combo binaries. No more lipo.
    Technically the previous version didn't actually support lipo, but had a
    stub where it should be added.
    
    Instead of going that route, this version just sticks everything in its own
    directory. This makes the combiner really easy and doesn't lose much, except
    maybe the cool factor of lipoing as much as possible.
    
    launch.rb now has to pay attention to the architecture as well as the
    version to figure out which emacs to run. It also now messes with the PATH
    to add the architecture bin and libexec dirs since the main Emacs code
    hardcodes them to Contents/MacOS/bin and Contents/MacOS/libexec,
    respectively.

--- launch.rb
+++ launch.rb
@@ -1,18 +1,38 @@
 #!/usr/bin/ruby
 
-def numify(version_string)
-  version_string.sub(/^(\d+)\.(\d+)(?:\.(\d+))?$/) { |m| $1.to_i*10000 + $2.to_i*100 + ($3 || "0").to_i*1 }.to_i
+class NumifiedVersion
+  attr_accessor :str, :num
+  def initialize(version_string)
+    @str = version_string
+    @num = @str.sub(/^(\d+)\.(\d+)(?:\.(\d+))?$/) { |m| $1.to_i*10000 + $2.to_i*100 + ($3 || "0").to_i*1 }.to_i
+  end
+  def to_s; @str; end
+  def ==(a);  return @num == a.num; end
+  def <=(a);  return @num <= a.num; end
+  def <=>(a); return @num <=> a.num; end
 end
+
 
 def osascript(script)
   system 'osascript', *script.split(/\n/).map { |line| ['-e', line] }.flatten
 end
 
+version = NumifiedVersion.new(`sw_vers -productVersion`)
+arch=`uname -m`.chomp
 
-versions = Hash[*Dir["#{File.dirname($0)}/Emacs-*"].map { |file| [ numify(file.sub(/^.*-(.+)$/, '\1')), file] }.flatten]
-version = numify(`sw_vers -productVersion`)
-if highest_compatible_version =  versions.keys.select { |v| v <= version }.max
-  exec [versions[highest_compatible_version], versions[highest_compatible_version]], *ARGV
+emacs = Dir["#{File.dirname($0)}/Emacs-*"].map { |file| file.match(/^.*-(.+)-(.+)$/) && {:arch=>$1, :version=>NumifiedVersion.new($2), :exe=>file} }
+        .select { |v| v[:arch] == arch && v[:version] <= version }
+        .sort { |a,b| a[:version] <=> b[:version] }
+        .last
+
+if emacs
+  # Emacs.app sticks Emacs.app/Contents/MacOS/{bin,libexec} on the end of the PATH when it starts, so if we
+  # stick our own architecture dependent paths on the end of the PATH then they will override Emacs's paths
+  # while not affecting any user paths.
+  base_dir=File.absolute_path(File.dirname($0))
+  ENV['PATH'] += File.join(base_dir,     "bin-#{emacs[:arch]}-#{emacs[:version]}") + ':' +
+                 File.join(base_dir, "libexec-#{emacs[:arch]}-#{emacs[:version]}")
+  exec [emacs[:exe], emacs[:exe]], *ARGV
 end
 
 osascript <<-ENDSCRIPT
