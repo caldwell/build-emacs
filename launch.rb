5426b9359e44bc979f233c6fcdc79b0039de3e15
David Caldwell <david@porkrind.org>
2014-07-21 12:06:55 -0700

    [launch.rb] Make sure the script runs on Ruby 1.8.7 (the system Ruby on 10.6)
    Fixes #28.

--- launch.rb
+++ launch.rb
@@ -9,16 +9,16 @@ end
 version = Gem::Version.new(`sw_vers -productVersion`)
 arch=`uname -m`.chomp
 
-emacs = Dir["#{File.dirname($0)}/Emacs-*"].map { |file| file.match(/^.*-(.+)-(.+)$/) && {:arch=>$1, :version=>Gem::Version.new($2), :exe=>file} }
-        .select { |v| v[:arch] == arch && v[:version] <= version }
-        .sort { |a,b| a[:version] <=> b[:version] }
+emacs = Dir["#{File.dirname($0)}/Emacs-*"].map { |file| file.match(/^.*-(.+)-(.+)$/) && {:arch=>$1, :version=>Gem::Version.new($2), :exe=>file} } \
+        .select { |v| v[:arch] == arch && v[:version] <= version } \
+        .sort { |a,b| a[:version] <=> b[:version] } \
         .last
 
 if emacs
   # Emacs.app sticks Emacs.app/Contents/MacOS/{bin,libexec} on the end of the PATH when it starts, so if we
   # stick our own architecture dependent paths on the end of the PATH then they will override Emacs's paths
   # while not affecting any user paths.
-  base_dir=File.absolute_path(File.dirname($0))
+  base_dir=File.expand_path(File.dirname($0))
   ENV['PATH'] += ':' + File.join(base_dir,     "bin-#{emacs[:arch]}-#{emacs[:version]}") +
                  ':' + File.join(base_dir, "libexec-#{emacs[:arch]}-#{emacs[:version]}")
   exec [emacs[:exe], emacs[:exe]], *ARGV
