dbafee3f8086608ad511da0007c9672ba9d9c38e
David Caldwell <david@porkrind.org>
2021-03-26 00:27:02 -0700

    Add "build-dependencies.rb" which downloads tarballs of various emacs dependencies and compiles them
    "build-dependencies" is a front-end for testing/debugging that isn't used by the build.

---
+++ build-dependencies.rb
@@ -0,0 +1,74 @@
+#  Copyright © 2021 David Caldwell
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'fileutils'
+require 'pathname'
+require 'pp'
+
+require_relative 'verbose-shell'
+require_relative 'build'
+
+class BuildDependencies
+
+  def initialize(prefix)
+    arm = `uname -m`.chomp == 'arm64'
+    kernel_version = `uname -r`.chomp
+    @prefix = File.expand_path(prefix)
+    @deps = [ # Ordered carefullt so dependencies work.
+      { source: "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.tar.gz", builddep: true,
+        extra_configure_args: [*%W"--with-internal-glib --disable-debug --disable-host-tool", "LIBS=-framework corefoundation -framework cocoa"] },
+
+      { source: "https://gmplib.org/download/gmp/gmp-6.2.1.tar.bz2" },
+      { source: "https://ftp.gnu.org/gnu/nettle/nettle-3.7.2.tar.gz",
+        extra_configure_args: %W"--disable-openssl" +
+                              (arm ? %W"--build=aarch64-apple-darwin#{kernel_version}" : []) },
+      { source: "https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz" },
+      { source: "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.15.tar.xz",
+        extra_configure_args: %W"--with-included-unistring --with-included-libtasn1 --without-p11-kit" },
+
+      { source: "https://github.com/libffi/libffi/releases/download/v3.3/libffi-3.3.tar.gz" }
+        .merge(arm ? { extra_configure_args: %W"--build=aarch64-apple-darwin#{kernel_version}" } : {}),
+
+      { source: "https://digip.org/jansson/releases/jansson-2.13.1.tar.bz2" },
+    ].map {|opts| Build.new(opts.merge(prefix: @prefix)) }
+  end
+
+  def clean
+    Vsh.rm_rf(@prefix)
+    @deps.each {|dep| dep.clean }
+  end
+
+  def add_to(key, val)
+    ENV[key] = "#{val}:#{ENV[key]}"
+  end
+
+  def ensure
+    add_to "PATH", "#{@prefix}/bin"
+    add_to "PKG_CONFIG_PATH", "#{@prefix}/lib/pkgconfig"
+    add_to "LIBRARY_PATH", "#{@prefix}/lib"
+    add_to "CPATH", "#{@prefix}/include"
+
+    @deps.each {|dep|
+      dep.fetch()
+      dep.build(@prefix)
+    }
+  end
+
+  def export_sources(dir)
+    Vsh.mkdir_p(dir)
+    @deps.each {|dep|
+      Vsh.ln(dep.archive_path, dir) unless dep.builddep
+    }
+  end
+end
---
+++ build.rb
@@ -0,0 +1,115 @@
+#!/usr/bin/env ruby
+#  Copyright © 2021 David Caldwell
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'net/http'
+require 'tempfile'
+require 'digest'
+
+require_relative 'verbose-shell'
+
+class Build
+    attr_accessor :source, :archive_name, :name, :version, :ext, :compress, :extra_configure_args, :extra_make_args, :builddep, :patches
+
+    def initialize(options)
+      @source = URI(options[:source])
+      @archive_name = File.basename(options[:source])
+      m = @archive_name.match(/^([\w-]+)-([\d.-]+)(\.tar.(\w+))$/)
+      raise "couldn't parse #{basename(@source)}" unless m
+      @name, @version, @ext, @compress = [m[1], m[2], m[3], m[4]]
+      @extra_configure_args = options[:extra_configure_args] || []
+      @extra_make_args = options[:extra_make_args] || []
+      @builddep = options[:builddep]
+      @patches = options[:patches] || []
+    end
+
+    def build_dir
+      File.join("build", "#{@name}-#{@version}")
+    end
+
+    def archive_path
+      File.join("archive", @archive_name)
+    end
+
+
+    def clean
+      Vsh.rm_rf(build_dir) if File.exist?(build_dir)
+    end
+
+    def prep_build_dir
+      Vsh.mkdir_p("build") unless File.exist? "build"
+      unless File.exist? build_dir
+        unpack(archive_path, build_dir)
+        patches.each {|patch| Vsh.system(*%W"patch -p0 -d #{build_dir} -i #{File.expand_path(patch)}") }
+      end
+    end
+
+    def fetch
+      download_url_to_file(archive_path, source) unless File.exist?(archive_path)
+    end
+
+    def build(prefix)
+      puts "==> Building #{@name}-#{@version}" if Vsh.verbose
+      prep_build_dir()
+      configure(prefix)
+      make()
+      install()
+    end
+
+    def configure(prefix)
+      configure_command = [*%W"./configure --prefix=#{File.absolute_path(prefix)}", *extra_configure_args]
+      conf_hash = Digest::SHA2.hexdigest(File.read(archive_path)+configure_command.join(' '))
+      return if File.exist? "#{build_dir}.configured" and File.read("#{build_dir}.configured") == conf_hash
+      Vsh.chdir(build_dir) {
+        Vsh.system(*configure_command)
+      }
+      File.write("#{build_dir}.configured", conf_hash)
+    end
+
+    def make
+      Vsh.chdir(build_dir) {
+        Vsh.system(*%W"make -j 4", *extra_make_args)
+      }
+    end
+
+    def install
+      Vsh.chdir(build_dir) {
+        Vsh.system(*%W"make install")
+      }
+    end
+
+    def download_url_to_file(dest, url)
+      puts "Downloading #{url}...\n" if Vsh.verbose
+      Vsh.mkdir_p(File.dirname(dest))
+      # Shelling out to curl is so gross. But the stupid ruby libs bundled on 10.11 and 10.12 are sslv3 (!!)
+      # so they can't actually do anything on the modern web.
+      Vsh.system *%W"curl -o #{dest}.downloading -L #{url}"
+      File.rename("#{dest}.downloading", dest)
+    end
+
+  def unpack(cache, dest)
+    Vsh.mkdir_p(File.dirname(dest))
+    Dir.mktmpdir(nil, File.dirname(dest)) do |unpackdir|
+      Vsh.system *%W"tar xf #{archive_path} -C #{unpackdir}"
+      Vsh.rm_rf dest
+      toplevel = Dir["#{unpackdir}/*"]
+      if toplevel.count == 1 and File.directory? toplevel[0]
+        Vsh.mv toplevel.first, dest
+      else
+        Vsh.mv unpackdir, dest
+        FileUtils.mkdir_p unpackdir # hack: put the dir back so mktmpdir can delete it
+      end
+    end
+  end
+end
