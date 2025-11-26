2cee0efb9f8e03b8c33dceced175e4b3eb4a646f
David Caldwell <david@porkrind.org>
2025-11-26 14:03:20 -0800

    dependencies.nix,build-emacs-from-tar: Pull dependency sources from nix into Emacs-*-dependencies.tar

--- dependencies.nix
+++ dependencies.nix
@@ -21,27 +21,77 @@ let
   ncurses-no-nix-store = pkgs.ncurses.overrideAttrs (finalAttrs: previousAttrs: {
     configureFlags = previousAttrs.configureFlags ++ [ "--with-terminfo-dirs=/usr/share/terminfo" ];
   });
-in
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.autoconf
-    pkgs.pkg-config or pkgs.pkgconfig
-    ncurses-no-nix-store
-    pkgs.zlib
-
+  deps = [
     pkgs.gnutls
     pkgs.jansson
     pkgs.libxml2
     pkgs.librsvg
     pkgs.tree-sitter
     pkgs.sqlite
-  ] ++ (
-    # This magic checks for the existence of darwin.apple_sdk.frameworks.AppKit which is only available in the
+  ];
+
+  dependency-details = (map (dep: {
+    name = dep.pname;
+    version = dep.version;
+    path = "${dep}";
+    nix_source = "${dep.src}";
+    source =
+      let archive-name = builtins.baseNameOf dep.src.resolvedUrl or dep.src.url;
+      in if (pkgs.lib.hasSuffix archive-name dep.src.outPath)
+         then archive-name
+         else "${dep.name}.tar.bz2";
+  }) deps);
+
+  dependencies.tar = pkgs.stdenv.mkDerivation rec {
+    name = "dependencies.tar";
+    buildInputs = [ pkgs.ruby ];
+    dontUnpack = true;
+    manifest = builtins.toJSON (map (deets: builtins.removeAttrs deets ["path"]) dependency-details);
+    buildPhase = ''
+      (set -x
+      mkdir dependencies
+      ruby -r json -r fileutils <<EOF
+        manifest=JSON.load(ENV['manifest'])
+        manifest.each do |dep|
+          if File.directory? dep['nix_source']
+            system(*%W"tar cjf dependencies/#{dep['name']}-#{dep['version']}.tar.bz2 -C #{dep['nix_source']} --transform s/\./#{dep['name']}-#{dep['version']}/ .")
+          else
+            FileUtils.cp dep['nix_source'], "dependencies/#{dep['source']}"
+          end
+        end
+        File.write("dependencies/manifest.json", manifest.to_json)
+      EOF
+      )
+    '';
+    installPhase = ''
+      tar cf "$out" dependencies
+    '';
+  };
+in
+
+pkgs.mkShell {
+  passthru = {
+    inherit pkgs deps dependency-details dependencies ncurses-no-nix-store system;
+  };
+
+  buildInputs = [
+    pkgs.autoconf
+    pkgs.pkg-config or pkgs.pkgconfig
+    ncurses-no-nix-store
+    pkgs.zlib
+
+  ] ++ deps
+  ++ (
+    # This magics check for the existence of darwin.apple_sdk.frameworks.AppKit which is only available in the
     # old nixpkgs we need to use for intel macs. In the newer nixpkgs, this isn't just nonexistent, it actively
     # throws, hence the weird tryEval stuff.
     if let e = { x = pkgs?darwin.apple_sdk.frameworks.AppKit; }; in (builtins.tryEval (builtins.deepSeq e e)).value?x
     then [pkgs.darwin.apple_sdk.frameworks.AppKit]
     else []
   );
+
+  # Publish some Nix details that build-emacs-from-tar can use
+  DEPENDENCY_DETAILS = builtins.toJSON dependency-details;
+  DEPENDENCIES_TAR = "${dependencies.tar}";
 }
