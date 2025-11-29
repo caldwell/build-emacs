6ec410ea956b36574c7fa74d1b4e0bad0bf6a687
David Caldwell <david@porkrind.org>
2025-11-29 15:23:34 -0800

    dependencies.nix: Move aarch64 dependencies forward to update all the dependencies (but mostly tree-sitter)
    In particular, this brings tree-sitter to 0.25.10 which supports API 15.

--- dependencies.nix
+++ dependencies.nix
@@ -1,10 +1,22 @@
 let
-  pkgs = import (builtins.fetchGit {
-      name = "emacs-dependencies-base";
-      url = "https://github.com/NixOS/nixpkgs/";
-      ref = "refs/heads/nixpkgs-unstable";
-      rev = "21eda9bc80bef824a037582b1e5a43ba74e92daa";
-    }) {};
+  system = {
+    x86_64-darwin = {
+      pkgs = nixpkgs_at_rev "21eda9bc80bef824a037582b1e5a43ba74e92daa";
+    };
+
+    aarch64-darwin = {
+      pkgs = nixpkgs_at_rev "a8d610af3f1a5fb71e23e08434d8d61a466fc942";
+    };
+  }.${builtins.currentSystem} or (throw "Unknown macOS system ${builtins.currentSystem}");
+
+  inherit (system) pkgs;
+
+  nixpkgs_at_rev = rev: import (builtins.fetchGit {
+    name = "emacs-dependencies-base";
+    url = "https://github.com/NixOS/nixpkgs/";
+    ref = "refs/heads/nixpkgs-unstable";
+    inherit rev;
+  }) {};
 
   ncurses-no-nix-store = pkgs.ncurses.overrideAttrs (finalAttrs: previousAttrs: {
     configureFlags = previousAttrs.configureFlags ++ [ "--with-terminfo-dirs=/usr/share/terminfo" ];
